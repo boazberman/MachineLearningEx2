7fbb3542dd90597c6a6f900830cbff7a5abd34d5
BoazB <berman.boaz@gmail.com>
2016-12-21 00:45:50 +0200

    Merge branch 'master' of https://github.com/saararbel/MachineLearningEx2
    # Conflicts:
    #	perceptron_2d.py

--- perceptron_2d.py
+++ perceptron_2d.py
@@ -1,8 +1,9 @@
+import math
 import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
-import math
+
 
 def import_data(dataSetFilePath):
     data = np.loadtxt(dataSetFilePath, delimiter=',')
@@ -13,50 +14,41 @@ def import_data(dataSetFilePath):
 
 
 def calc_r(x):
-    R = 0
+    r = 0
     for idx, xi in enumerate(x):
-        square_size = sum([x*x for x in xi.tolist()])
-        if math.sqrt(square_size) > R:
-            R = math.sqrt(square_size)
+        square_size = sum(x ** 2 for x in xi.tolist())
+        if math.sqrt(square_size) > r:
+            r = math.sqrt(square_size)
 
-    return R
+    return r
 
 
-def calcGama(x, y, normalize_w):
+def gamma(x, y, normalize_w):
     gama = sys.float_info.max
     for idx, xi in enumerate(x):
         temp = np.dot([1] + xi.tolist(), normalize_w) * y[idx]
-        if  temp < gama:
+        if temp < gama:
             gama = temp * y[idx]
 
     return gama
 
 
-def cartezian_product(x, w):
-    sum = 0
-    for i, e in enumerate(x):
-        sum += e * w[i]
-    return sum
-
-
-def perceptronAlgo(x, y, learningRate=1):
-    weight_vector = [0] * (x[0].size + 1)
-    input = []
-    bias = 1
+def perceptron(x, y, learningRate=1):
     mistakes, iterations = 0, 0
-    weights_changed = True
-    while weights_changed:
-        weights_changed = False
+    should_continue = True
+    w = [0] * (x[0].size + 1)
+    while should_continue:
+        should_continue = False
         iterations += 1
         for idx, xi in enumerate(x):
-            xi_list = [bias] + xi.tolist()
-            if np.dot(xi_list, weight_vector) * y[idx] <= 0:
-                weight_vector = [(y[idx] * xi_list[w_index] * learningRate) + elem for w_index, elem in
-                                 enumerate(weight_vector)]
-                weights_changed = True
+            xi_list = [1] + xi.tolist()
+            if np.dot(xi_list, w) * y[idx] <= 0:
+                w = [(y[idx] * xi_list[w_index] * learningRate) + elem for w_index, elem in
+                     enumerate(w)]
+                should_continue = True
                 mistakes += 1
 
-    return weight_vector, mistakes, iterations
+    return w, mistakes, iterations
 
 
 def split_by_sign(y):
@@ -74,26 +66,22 @@ def plot_data(x, y):
     plt.show()
 
 
-def vector_normalize(w):
-    size = math.sqrt(sum([x*x for x in w]))
-    return [float(x/size) for x in w]
+def normalize(vector):
+    return [float(x / math.sqrt(sum(x * x for x in vector))) for x in vector]
 
 
 def calc_mistake_bound():
-    r = calc_r(x)
-    gama = calcGama(x, y, vector_normalize(w))
-    return (r / gama) * (r / gama)
+    return (calc_r(x) / gamma(x, y, normalize(w))) ** 2
 
 
 if __name__ == '__main__':
     x, y = import_data(sys.argv[1])
     # plot_data(x, y)
-    w, mistakes, iterations = perceptronAlgo(x, y, 0.39)
+    w, mistakes, iterations = perceptron(x, y)
     # plot_separation_hyperplane(w,x)
 
-    outputFile = open("output.txt", 'w')
-    outputFile.write("output1: " + str(w) + "\n")
-    outputFile.write("output2: " + str(mistakes) + "\n")
-    outputFile.write("output3: " + str(iterations) + "\n")
-    outputFile.write("output4: " + str(calc_mistake_bound()) + "\n")
-
+    with open("output.txt", 'w') as outputFile:
+        outputFile.write("output1: %s\n" % w)
+        outputFile.write("output2: %s\n" % mistakes)
+        outputFile.write("output3: %s\n" % iterations)
+        outputFile.write("output4: %s\n" % calc_mistake_bound())
